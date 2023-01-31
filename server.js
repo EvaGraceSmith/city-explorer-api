c4e1316b6da83ee232cc1c5ad90ce8c9c8d3b516
EvaGraceSmith <smithevagrace@gmail.com>
2023-01-31 00:12:53 -0600

    added forcast class

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ require('dotenv').config();
 let data = require('./data/weather.json');
 const cors = require('cors');
 
+
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT;
+
 
 app.get('/', (request, response)=>{
   response .send('Hello from our server HOME route/ !!');
@@ -30,38 +32,64 @@ app.get('/hello',(request, response)=>{
 
 
 app.get('/weather', (request, response)=>{
-  //http://localhost3003/city?cityname=Paris
+  //http://localhost3003/weather?cityname=Seattle&&citylon="-122.33207"&&citylat="47.60621"
   try{
     let cityName = request.query.cityname;
     let cityLat = request.query.citylat;
     let cityLon = request.query.citylon;
 
+
     // let dataToSend = data.find(city => city.cityname === cityName);
-    // console.log('did we find city name?', dataToSend);
-    let dataToInstantiate = data.find(city => city.cityname === cityName &&
-        city.citylat === cityLat && city.citylon === cityLon );
-    let dataToSend = new Weather(dataToInstantiate);
-    console.log(dataToSend, 'got back from weather class');
-    response.status(200).send(dataToSend);
+    console.log('did we find city name?', cityName, cityLat,cityLon);
+    let dataToInstantiate = data.find(city => city.city_name === cityName); //&&
+       // city.citylat === cityLat && city.citylon === cityLon );
+    if (dataToInstantiate === undefined){
+      response.status(500).send('City not found');
+    }
+    else{
+      let dataToSend = new Forecast(dataToInstantiate);
+      console.log(dataToSend, 'got back from weather class');
+      let returnObject= [];
+      returnObject.push({'date':dataToSend.datetime});
+      returnObject.push({'description':`Low of ${dataToSend.lowTemp}, high of ${dataToSend.maxTemp} with ${dataToSend.description}`});
+      console.log(returnObject, 'got back to send to weather client');
+      //   [
+      //     {
+      //       "description": "Low of 17.1, high of 23.6 with broken clouds",
+      //       "date": "2021-03-31"
+      //     },
+      //     {
+      //       "description": "Low of 17.5, high of 29.9 with few clouds",
+      //       "date": "2021-04-01"
+      //     },
+      //     ...
+      //   ]
+      response.status(200).send(returnObject);
+    }
 
   } catch (error){
     //create a new instance of error
     // this will instantiate any new error
-    next(error);
+    response.status(500).send(error.message);
   }
 });
+
 
 //The order for these messages is important
 app.get('*', (request, response)=>{
   response.status(404).send('The route was not found. Error 404');
 });
 
+
 //class
-class Weather{
+class Forecast{
   constructor(weatherObject){
-    console.log('did we get an object to construct?');
-    this.cityName = weatherObject.pizzatype;
-    this.location = weatherObject.location;
+    console.log('did we get an object to construct?',weatherObject.data[0].weather);
+    this.cityName = weatherObject.city_name;
+    this.datetime = weatherObject.data[0].datetime;
+    this.description = weatherObject.data[0].weather.description;
+    this.lowTemp = weatherObject.data[0].low_temp;
+    this.maxTemp = weatherObject.data[0].max_temp;
   }
 }
 
@@ -69,7 +97,7 @@ class Weather{
 
 // eslint-disable-next-line no-unused-vars
 app.use((error, request, response, next) =>{
-  response.statues(500).send(error.message);
+  response.status(500).send(error.message);
 });
 
 
